452a5b769fb0d5b6b635ad4683457893325e9e92
Julio Avila <javila3090@gmail.com>
2017-10-10 09:50:44 -0400

    SWFacts v1.0

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -106,7 +106,7 @@
         <div class="modal-dialog">
             <!-- Modal content-->
             <div class="modal-content">
-                <div class="modal-header">
+                <div class="modal-header"> 
                     <button type="button" class="close" data-dismiss="modal">&times;</button>
                     <h4 class="modal-title">Payment Details</h4>
                 </div>
